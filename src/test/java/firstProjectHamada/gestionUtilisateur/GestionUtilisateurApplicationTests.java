1b74b3eda372468b5f4253055ae1db406cfecd03
Hamada976 <hamada.fayadhu@gmail.com>
2022-08-22 15:08:18 +0200

    first commit

---
+++ src/test/java/firstProjectHamada/gestionUtilisateur/GestionUtilisateurApplicationTests.java
@@ -0,0 +1,13 @@
+package firstProjectHamada.gestionUtilisateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUtilisateurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
